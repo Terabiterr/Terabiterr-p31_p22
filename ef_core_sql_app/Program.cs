59494b8cb8eca47238d908f2bdb4761d70e800d9
Mario <mario@gmail.com>
2025-02-18 18:53:12 +0200

    committtt

--- ef_core_sql_app/Program.cs
+++ ef_core_sql_app/Program.cs
@@ -1,10 +1,22 @@
-﻿namespace ef_core_sql_app
+﻿using ef_core_sql_app.Models;
+using ef_core_sql_app.Services;
+using System;
+
+namespace ef_core_sql_app
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Blogging blogging = new Blogging();
+            using (var serviceBlog = new ServiceBlog(new Blogging()))
+            {
+                //serviceBlog.CreateBlog(new Blog { Name = "Blog 1" });
+                var blogs = serviceBlog.GetBlogs();
+                foreach (var item in blogs)
+                {
+                    Console.WriteLine(item.Name);
+                }
+            }
         }
     }
 }
---
+++ ef_core_sql_app/Services/ServiceBlog.cs
@@ -0,0 +1,79 @@
+﻿using ef_core_sql_app.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ef_core_sql_app.Services
+{
+    public interface IServiceBlog
+    {
+        void CreateBlog(Blog blog);
+        IEnumerable<Blog> GetBlogs();
+        Blog GetBlogById(int id);
+        void UpdateBlog(int id, Blog blog);
+        void DeleteBlogById(int id);
+        void AddPostToBlog(int blogId, Post post);
+        void RemovePostFromBlog(int postId);
+        IEnumerable<Post> GetPostsByBlogId(int blogId);
+        void UpdatePostById(int postId);
+    }
+    public class ServiceBlog : IServiceBlog, IDisposable
+    {
+        private readonly Blogging context;
+        public ServiceBlog(Blogging context)
+        {
+            this.context = context;
+        }
+
+        public void AddPostToBlog(int blogId, Post post)
+        {
+            
+        }
+
+        public void CreateBlog(Blog blog)
+        {
+            context.Database.ExecuteSqlCommand("INSERT INTO Blogs(Name) VALUES({0})", blog.Name);
+        }
+
+        public void DeleteBlogById(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+            context.Dispose();
+        }
+
+        public Blog GetBlogById(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IEnumerable<Blog> GetBlogs()
+        {
+            return context.Blogs.SqlQuery("SELECT * FROM Blogs")
+                .ToList();
+        }
+
+        public IEnumerable<Post> GetPostsByBlogId(int blogId)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void RemovePostFromBlog(int postId)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void UpdateBlog(int id, Blog blog)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void UpdatePostById(int postId)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
